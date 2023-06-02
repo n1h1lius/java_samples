00561635efaf4b8000693cebcf6bd77bc8aa147b
n1h1lius <135299351+n1h1lius@users.noreply.github.com>
2023-06-02 06:13:36 +0200

    Create java_example_comments.js

---
+++ introduction_guide/java_example_comments.js
@@ -0,0 +1,16 @@
+public class Example {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, world!"); // This is another single-line comment
+
+        /* This is a
+           multi-line comment */
+        
+        /*
+         * This is another
+         * multi-line comment
+         */
+
+        // System.out.println("This line won't be executed");
+    }
+}
